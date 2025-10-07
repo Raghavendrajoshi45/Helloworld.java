b5c085bc8f11e831b52495b73d79492dfa96e668
Raghavendrajoshi45 <74344432+Raghavendrajoshi45@users.noreply.github.com>
2025-10-07 14:19:13 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,6 @@
-public class Helloworld{
-
-public static void main(String[] args) { 
-
-System.out.println("Hello World");
-
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
-}
+
